c110bd550a539c9fdf90d5cbeb5af7b6e22183fa
Oleksandr Z <cyvubino@protonmail.com>
2021-10-25 17:49:29 +0300

    added tests for item market

--- test/marketWithProxy.js
+++ test/marketWithProxy.js
@@ -4,6 +4,7 @@ const { constants, utils } = require('ethers');
 
 describe('marketWithProxy', function() {
   const ganache = new Ganache();
+  const fee = 9;
 
   let accounts;
   let owner;
@@ -63,7 +64,7 @@ describe('marketWithProxy', function() {
     marketLogic.connect(owner).initialize(constants.AddressZero, constants.AddressZero, utils.parseEther('0'));
 
     const ProxyMarket = await ethers.getContractFactory('contracts/market/ScarcityCraftingIMarketProxy.sol:RarityCraftingIMarketProxy');
-    let data = marketLogic.interface.encodeFunctionData("initialize", [crafting.address, wrapped_gold.address, '9']);
+    let data = marketLogic.interface.encodeFunctionData("initialize", [crafting.address, wrapped_gold.address, fee]);
     proxyMarket = await ProxyMarket.deploy(marketLogic.address, proxyAdmin.address, data);   
 
     market = Market.attach(proxyMarket.address);
@@ -116,6 +117,13 @@ describe('marketWithProxy', function() {
     expect(await market.listLength()).equal(1);
   });
 
+  it('should be NOT possible to list item on market with 0 price', async ()=> {
+    let itemId = crafting.tokenOfOwnerByIndex(user.address, 0);
+    let price = utils.parseEther('0');
+    await crafting.connect(user).approve(market.address, itemId);
+    await expect(market.connect(user).list(itemId, price)).to.be.revertedWith('bad price');
+  });
+
   it('should be possible to unlist item from market', async ()=> {
     let itemId = crafting.tokenOfOwnerByIndex(user.address, 0);
     let price = utils.parseEther('100');
@@ -129,6 +137,17 @@ describe('marketWithProxy', function() {
     expect(await market.listLength()).equal(0);
   });
 
+  it('should be NOT possible to unlist foreign item from market', async ()=> {
+    let itemId = crafting.tokenOfOwnerByIndex(user.address, 0);
+    let price = utils.parseEther('100');
+    await crafting.connect(user).approve(market.address, itemId);
+    await market.connect(user).list(itemId, price);
+    expect(await crafting.ownerOf(itemId)).equal(market.address);
+    expect(await market.listLength()).equal(1);
+
+    await expect(market.connect(receiver).unlist(itemId)).to.be.revertedWith('not lister');
+  });
+
   it('should be possible to buy item from market', async ()=> {
     let itemId = crafting.tokenOfOwnerByIndex(user.address, 0);
     let price = utils.parseEther('250');
@@ -137,14 +156,120 @@ describe('marketWithProxy', function() {
     expect(await crafting.ownerOf(itemId)).equal(market.address);
     expect(await market.listLength()).equal(1);
 
-
     await gold.connect(user).approve(summon_id, await wrapped_gold.SUMMMONER_ID(), price);
     await wrapped_gold.connect(user).deposit(summon_id, price);
     await wrapped_gold.connect(user).transfer(receiver.address, price);
+
+    balanceUser = await wrapped_gold.balanceOf(user.address);
+
     await wrapped_gold.connect(receiver).approve(market.address, price);
     await market.connect(receiver).buy(itemId);
     expect(await crafting.ownerOf(itemId)).equal(receiver.address);
     expect(await market.listLength()).equal(0);
+    expect(await wrapped_gold.balanceOf(receiver.address)).equal(0);
+    expect(await wrapped_gold.balanceOf(user.address)).equal((price - price * fee / 10000).toString());
+    expect(await wrapped_gold.balanceOf(market.address)).equal((price * fee / 10000).toString());
+  });
+
+  it('should be NOT possible to buy item from market with lower approve', async ()=> {
+    let itemId = crafting.tokenOfOwnerByIndex(user.address, 0);
+    let price = utils.parseEther('250');
+    await crafting.connect(user).approve(market.address, itemId);
+    await market.connect(user).list(itemId, price);
+    expect(await crafting.ownerOf(itemId)).equal(market.address);
+    expect(await market.listLength()).equal(1);
+
+    await gold.connect(user).approve(summon_id, await wrapped_gold.SUMMMONER_ID(), price);
+    await wrapped_gold.connect(user).deposit(summon_id, price);
+    await wrapped_gold.connect(user).transfer(receiver.address, price);
+
+    balanceUser = await wrapped_gold.balanceOf(user.address);
+
+    await wrapped_gold.connect(receiver).approve(market.address, utils.parseEther('249'));
+    await expect(market.connect(receiver).buy(itemId)).to.be.revertedWith('ERC20: transfer amount exceeds allowance');
+  });
+
+  it('should be possible to withdraw fees be admin', async ()=> {
+    let itemId = crafting.tokenOfOwnerByIndex(user.address, 0);
+    let price = utils.parseEther('250');
+    await crafting.connect(user).approve(market.address, itemId);
+    await market.connect(user).list(itemId, price);
+    expect(await crafting.ownerOf(itemId)).equal(market.address);
+    expect(await market.listLength()).equal(1);
+
+    await gold.connect(user).approve(summon_id, await wrapped_gold.SUMMMONER_ID(), price);
+    await wrapped_gold.connect(user).deposit(summon_id, price);
+    await wrapped_gold.connect(user).transfer(receiver.address, price);
+
+    balanceUser = await wrapped_gold.balanceOf(user.address);
+
+    await wrapped_gold.connect(receiver).approve(market.address, price);
+    await market.connect(receiver).buy(itemId);
+
+    let fees = (price * fee / 10000).toString()
+
+    expect(await wrapped_gold.balanceOf(market.address)).equal(fees);
+    await market.connect(owner).withdraw(fees);
+    expect(await wrapped_gold.balanceOf(owner.address)).equal(fees);
+  });
+
+  it('should be NOT possible to withdraw fees be not admin', async ()=> {
+    let itemId = crafting.tokenOfOwnerByIndex(user.address, 0);
+    let price = utils.parseEther('250');
+    await crafting.connect(user).approve(market.address, itemId);
+    await market.connect(user).list(itemId, price);
+    expect(await crafting.ownerOf(itemId)).equal(market.address);
+    expect(await market.listLength()).equal(1);
+
+    await gold.connect(user).approve(summon_id, await wrapped_gold.SUMMMONER_ID(), price);
+    await wrapped_gold.connect(user).deposit(summon_id, price);
+    await wrapped_gold.connect(user).transfer(receiver.address, price);
+
+    balanceUser = await wrapped_gold.balanceOf(user.address);
+    await wrapped_gold.connect(receiver).approve(market.address, price);
+    await market.connect(receiver).buy(itemId);
+    let fees = (price * fee / 10000).toString()
+
+    expect(await wrapped_gold.balanceOf(market.address)).equal(fees);
+    await expect(market.connect(receiver).withdraw(fees)).to.be.revertedWith('!owner');
+  });
+
+  it('should be NOT possible to withdraw more fees than collected', async ()=> {
+    let itemId = crafting.tokenOfOwnerByIndex(user.address, 0);
+    let price = utils.parseEther('250');
+    await crafting.connect(user).approve(market.address, itemId);
+    await market.connect(user).list(itemId, price);
+    expect(await crafting.ownerOf(itemId)).equal(market.address);
+    expect(await market.listLength()).equal(1);
+
+    await gold.connect(user).approve(summon_id, await wrapped_gold.SUMMMONER_ID(), price);
+    await wrapped_gold.connect(user).deposit(summon_id, price);
+    await wrapped_gold.connect(user).transfer(receiver.address, price);
+
+    balanceUser = await wrapped_gold.balanceOf(user.address);
+    await wrapped_gold.connect(receiver).approve(market.address, price);
+    await market.connect(receiver).buy(itemId);
+    let fees = (price * (fee + 1) / 10000).toString()
+
+    await expect(market.connect(owner).withdraw(fees)).to.be.revertedWith('ERC20: transfer amount exceeds balance');
+  });
+
+  it('should be NOT possible to buy item that not listed on the market', async ()=> {
+    let itemId = crafting.tokenOfOwnerByIndex(user.address, 0);
+    let price = utils.parseEther('250');
+    await crafting.connect(user).approve(market.address, itemId);
+    await market.connect(user).list(itemId, price);
+    expect(await crafting.ownerOf(itemId)).equal(market.address);
+    expect(await market.listLength()).equal(1);
+
+    await gold.connect(user).approve(summon_id, await wrapped_gold.SUMMMONER_ID(), price);
+    await wrapped_gold.connect(user).deposit(summon_id, price);
+    await wrapped_gold.connect(user).transfer(receiver.address, price);
+
+    balanceUser = await wrapped_gold.balanceOf(user.address);
+
+    await wrapped_gold.connect(receiver).approve(market.address, price);
+    await expect(market.connect(receiver).buy('1000')).to.be.revertedWith('not listed');
   });
 
 });
